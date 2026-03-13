dd5a284206ea093ba182d6fbe7b6460439ede5f7
Mukhil <mukhiljjsa@gmail.com>
2026-03-13 17:03:26 +0530

    connected frontend to backend api

--- expense-tracker-frontend/src/services/api.js
+++ expense-tracker-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: https://expense-tracker-fullstack-1-ikle.onrender.com,
 });
 
 export default API;
